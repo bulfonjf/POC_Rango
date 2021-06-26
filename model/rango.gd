7efd810a836a07ec2ba21908443c10b0fb104e77
Cofla <alexis_g90@hotmail.com>
2021-06-26 03:01:20 -0300

    WIP Agregado add_steps_optimos a Rango

--- model/rango.gd
+++ model/rango.gd
@@ -1,4 +1,12 @@
 class_name Rango
 
+var steps_optimos : StepsOptimos
+
 func _init():
     return
+
+func add_steps_optimos(_steps_optimos : StepsOptimos):
+    steps_optimos = _steps_optimos
+
+func _eq(_rango : Rango) -> bool:
+    return true
--- tests/unit/test_rango.gd
+++ tests/unit/test_rango.gd
@@ -9,22 +9,22 @@ func test_init():
 	assert_is(resultado_obtenido, Rango)
 
 func test_add_steps_optimos():
-    #Arrange
-    var sut = Rango.new()
-    var steps_optimos = StepsOptimos.new([
+	#Arrange
+	var sut = Rango.new()
+	var steps_optimos = StepsOptimos.new([
 		Step.new(Vector2(0,4), 0), 
 		Step.new(Vector2(-1,4), 1),
-        Step.new(Vector2(1,4), 1)
+		Step.new(Vector2(1,4), 1)
 	])
-    var steps_esperados = StepsOptimos.new([
+	var steps_esperados = StepsOptimos.new([
 		Step.new(Vector2(0,4), 0), 
 		Step.new(Vector2(-1,4), 1),
-        Step.new(Vector2(1,4), 1)
+		Step.new(Vector2(1,4), 1)
 	])
-    
-    #Act
-    var resultado_esperado = steps_esperados
-    var resultado_obtenido = Rango.add_steps_optimos(steps_optimos)
 
-    #Assert
-    assert_
+	#Act
+	sut.add_steps_optimos(steps_optimos)
+	var resultado_esperado = steps_esperados
+	
+	#Assert
+	assert_true(sut.steps_optimos._eq(resultado_esperado))
